bc207a31579e452198e7872e8d70ed1db81563a9
chuang85 <chuang85@illinois.edu>
2014-06-11 20:36:05 -0700

    add model controller

---
+++ Onboarding/Controllers/ServicePrincipalTemplateController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Onboarding.Controllers
+{
+    public class ServicePrincipalTemplateController : ApiController
+    {
+        // GET api/serviceprincipaltemplate
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/serviceprincipaltemplate/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/serviceprincipaltemplate
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/serviceprincipaltemplate/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/serviceprincipaltemplate/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Onboarding/Models/ServicePrincipalTemplate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Onboarding.Models
+{
+    public class ServicePrincipalTemplate
+    {
+        public string name { get; set; }
+
+        public string environment { get; set; }
+    }
+}
