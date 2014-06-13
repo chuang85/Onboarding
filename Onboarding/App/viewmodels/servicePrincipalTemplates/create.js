7775d355f33bbda5277fe371ed1fc999622d132e
chuang85 <chuang85@illinois.edu>
2014-06-13 15:54:45 -0700

    intellisense working

--- Onboarding/App/viewmodels/servicePrincipalTemplates/create.js
+++ Onboarding/App/viewmodels/servicePrincipalTemplates/create.js
@@ -1,35 +1,26 @@
-﻿define(['durandal/system', 'durandal/app', 'plugins/router', 'repositories/sptsRepository'],
-    function (system, app, router, sptsRepository) {
-        return {
-            sptToAdd: {
-                name: ko.observable(),
-                environment: ko.observable()
-            },
+﻿define(['durandal/system', 'durandal/app', 'plugins/router'],
+    function (system, app, router) {
 
-            activate: function () {
-                this.sptToAdd.name("");
-                this.sptToAdd.environment("");
-                this._sptToAdd = false;
-            },
-
-            //canDeactivate: function () {
-            //    if (this._sptToAdd == false) {
-            //        return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
-            //    } else {
-            //        return true;
-            //    }
-            //},
-
-            createSPT: function () {
-                // Add spt to db
-                sptsRepository.createSpts(ko.toJS(this.sptToAdd));
-
-                // flag new spt
-                this._sptToAdd = true;
-                toastr.info('Create successfully');
-                // return to list of spts
-                router.navigate("");
-            }
+        var vm = {
+            name: ko.observable(),
+            environment: ko.observable(),
+            activate: activate
         };
+
+        function activate() {
+            this.name("");
+            this.environment("");
+            this._sptToAdd = false;
+        };
+
+        //canDeactivate: function () {
+        //    if (this._sptToAdd == false) {
+        //        return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
+        //    } else {
+        //        return true;
+        //    }
+        //},
+
+        return vm;
 
     });
---
+++ Onboarding/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="bootstrap.js" />
+/// <reference path="breeze.debug.js" />
+/// <reference path="jquery-1.9.1.js" />
+/// <reference path="knockout-3.1.0.debug.js" />
+/// <reference path="q.js" />
+/// <reference path="toastr.js" />
