b5ba5d60bdf13d653a3f4b09b794aa7fa4aeb4dc
Chengkan Huang <t-chehu@microsoft.com>
2014-07-03 18:42:33 -0700

    datetime fixed?!

--- Onboarding/Utils/OnboardingContextProvider.cs
+++ Onboarding/Utils/OnboardingContextProvider.cs
@@ -27,35 +27,36 @@ namespace Onboarding.Utils
 
         protected override bool BeforeSaveEntity(EntityInfo entityInfo)
         {
-            // Convert time format from UTC to local.
             OnboardingRequest onboardingRequest = (OnboardingRequest)entityInfo.Entity;
-            onboardingRequest.CreatedDate = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.Local);
 
-            // Write string formatted xml in to an xml file and assign it to Blob field.
-            string filename = onboardingRequest.DisplayName + "_" + onboardingRequest.CreatedBy + ".xml";
-            onboardingRequest.Blob = SystemHelpers.SavesStringToXml(onboardingRequest.TempXmlStore, SavingPathXml, filename);
+            // Set create time to Now.
+            onboardingRequest.CreatedDate = DateTime.UtcNow;
+
+            //// Write string formatted xml in to an file and assign it to Blob field.
+            //string filename = onboardingRequest.DisplayName + "_" + onboardingRequest.CreatedBy + ".xml";
+            //onboardingRequest.Blob = SystemHelpers.SavesStringToXml(onboardingRequest.TempXmlStore, SavingPathXml, filename);
             
-            // Handle file copy and add to (local) source depot operations.
-            RunCmd(filename);
+            //// Handle file copy and add to (local) source depot operations.
+            //RunCmd(filename);
 
-            // Step 1 - Create a code review
-            // Assign ReviewId to the corresponding field in OnboardingRequest
-            onboardingRequest.CodeFlowId = CodeFlowHelpers.CreateReview(rClient, onboardingRequest.CreatedBy, "Chengkan Huang",
-                onboardingRequest.CreatedBy + "@microsoft.com", "Testing integration", "MSODS");
+            //// Step 1 - Create a code review
+            //// Assign ReviewId to the corresponding field in OnboardingRequest
+            //onboardingRequest.CodeFlowId = CodeFlowHelpers.CreateReview(rClient, onboardingRequest.CreatedBy, "Chengkan Huang",
+            //    onboardingRequest.CreatedBy + "@microsoft.com", "Testing integration", "MSODS");
 
-            // Step 2 - Create a code package and add it to the review
-            CodeFlowHelpers.AddCodePackage(rClient, onboardingRequest.CodeFlowId, CodeFlowHelpers.CreateCodePackage("testing pack", onboardingRequest.CreatedBy, onboardingRequest.CreatedBy, CodePackageFormat.SourceDepotPack, new Uri(DestPath + filename)));
-            //CodeFlowHelpers.AddCodePackage(rClient, onboardingRequest.CodeFlowId, CodeFlowHelpers.CreateCodePackage("testing pack", onboardingRequest.CreatedBy, onboardingRequest.CreatedBy, new Uri(DestPath + filename)));
+            //// Step 2 - Create a code package and add it to the review
+            //CodeFlowHelpers.AddCodePackage(rClient, onboardingRequest.CodeFlowId, CodeFlowHelpers.CreateCodePackage("testing pack", onboardingRequest.CreatedBy, onboardingRequest.CreatedBy, CodePackageFormat.SourceDepotPack, new Uri(DestPath + filename)));
+            ////CodeFlowHelpers.AddCodePackage(rClient, onboardingRequest.CodeFlowId, CodeFlowHelpers.CreateCodePackage("testing pack", onboardingRequest.CreatedBy, onboardingRequest.CreatedBy, new Uri(DestPath + filename)));
 
 
-            // Step 3 - Add reviewers to the review
-            CodeFlowHelpers.AddReviewers(rClient, onboardingRequest.CodeFlowId, new ReviewService.Reviewer[]
-                {
-                    CodeFlowHelpers.CreateReviewer(onboardingRequest.CreatedBy, "Chengkan Huang", onboardingRequest.CreatedBy + "@microsoft.com", true)
-                });
+            //// Step 3 - Add reviewers to the review
+            //CodeFlowHelpers.AddReviewers(rClient, onboardingRequest.CodeFlowId, new ReviewService.Reviewer[]
+            //    {
+            //        CodeFlowHelpers.CreateReviewer(onboardingRequest.CreatedBy, "Chengkan Huang", onboardingRequest.CreatedBy + "@microsoft.com", true)
+            //    });
 
-            // Step 4 - Publish the review
-            CodeFlowHelpers.PublishReview(rClient, onboardingRequest.CodeFlowId, "meesage from author");
+            //// Step 4 - Publish the review
+            //CodeFlowHelpers.PublishReview(rClient, onboardingRequest.CodeFlowId, "meesage from author");
 
             // Close clients
             rClient.Close();
