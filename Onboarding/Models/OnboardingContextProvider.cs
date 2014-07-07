3693fa867cac31d60c737b8a1b627c21a280174f
Chengkan Huang <t-chehu@microsoft.com>
2014-07-07 11:30:56 -0700

    refactoring

--- Onboarding/Models/OnboardingContextProvider.cs
+++ Onboarding/Models/OnboardingContextProvider.cs
@@ -17,72 +17,70 @@ namespace Onboarding.Models
 {
     public class OnboardingRequestContextProvider : EFContextProvider<OnboardingDbContext>
     {
-        private const string CmdPath = @"C:\WINDOWS\system32\cmd.exe";
-        private const string CmdConfigArgs = @"/k set inetroot=e:\cumulus_main&set corextbranch=main&e:\cumulus_main\tools\path1st\myenv.cmd";
-        private const string SourcePath = @"C:\Users\t-chehu\Source\Repos\Onboarding\Onboarding\App_Data\";
-        private const string DestPath = @"E:\CUMULUS_MAIN\sources\dev\RestServices\GraphService\Tools\";
-        private const string SavingPathXml = @"../../App_Data/";
-
         public ReviewDashboardServiceClient qClient = new ReviewDashboardServiceClient();
         public ReviewServiceClient rClient = new ReviewServiceClient();
 
         protected override bool BeforeSaveEntity(EntityInfo entityInfo)
         {
-            if (entityInfo.EntityState == EntityState.Added && 
+            if (entityInfo.EntityState == EntityState.Added &&
                 entityInfo.Entity is OnboardingRequest)
             {
                 // Get the instance of the OnboardingRequest class.
                 var onboardingRequest = (OnboardingRequest)entityInfo.Entity;
 
                 // Set CreatedDate & ModifiedDate to Now.
-                onboardingRequest.CreatedDate = DateTime.UtcNow;
-                onboardingRequest.DisplayCreatedDate = onboardingRequest.CreatedDate.ToLocalTime().ToString("M/d/yyyy h:mm tt",
-                      CultureInfo.CreateSpecificCulture("en-US"));
-                onboardingRequest.ModifiedDate = onboardingRequest.CreatedDate;
-                onboardingRequest.DisplayModifiedDate = onboardingRequest.DisplayCreatedDate;
+                SetCreatedTimeOnInitialization(onboardingRequest);
 
-                //// Write string formatted xml in to an file and assign it to Blob field.
-                //string filename = onboardingRequest.DisplayName + "_" + onboardingRequest.CreatedBy + ".xml";
-                //onboardingRequest.Blob = SystemHelpers.SavesStringToXml(onboardingRequest.TempXmlStore, SavingPathXml, filename);
+                // Write string formatted xml in to an file and assign it to Blob field.
+                string filename = SetSptFilename(onboardingRequest);
+                onboardingRequest.Blob = SystemHelpers.SavesStringToXml(onboardingRequest.TempXmlStore, filename);
 
-                //// Handle file copy and add to (local) source depot operations.
-                //RunCmd(filename);
+                // Handle file copy and add to (local) source depot operations.
+                SystemHelpers.RunCmd(filename);
 
-                //// Step 1 - Create a code review
-                //// Assign ReviewId to the corresponding field in OnboardingRequest
-                //onboardingRequest.CodeFlowId = CodeFlowHelpers.CreateReview(rClient, onboardingRequest.CreatedBy, "Chengkan Huang",
-                //    onboardingRequest.CreatedBy + "@microsoft.com", "Testing integration", "MSODS");
-
-                //// Step 2 - Create a code package and add it to the review
-                //CodeFlowHelpers.AddCodePackage(rClient, onboardingRequest.CodeFlowId, CodeFlowHelpers.CreateCodePackage("testing pack", onboardingRequest.CreatedBy, onboardingRequest.CreatedBy, CodePackageFormat.SourceDepotPack, new Uri(DestPath + filename)));
-                ////CodeFlowHelpers.AddCodePackage(rClient, onboardingRequest.CodeFlowId, CodeFlowHelpers.CreateCodePackage("testing pack", onboardingRequest.CreatedBy, onboardingRequest.CreatedBy, new Uri(DestPath + filename)));
-
-
-                //// Step 3 - Add reviewers to the review
-                //CodeFlowHelpers.AddReviewers(rClient, onboardingRequest.CodeFlowId, new ReviewService.Reviewer[]
-                //    {
-                //        CodeFlowHelpers.CreateReviewer(onboardingRequest.CreatedBy, "Chengkan Huang", onboardingRequest.CreatedBy + "@microsoft.com", true)
-                //    });
-
-                //// Step 4 - Publish the review
-                //CodeFlowHelpers.PublishReview(rClient, onboardingRequest.CodeFlowId, "meesage from author");
+                SubmitCodeReviewFromRequest(onboardingRequest, filename);
 
                 // Close clients
                 rClient.Close();
-                qClient.Close();
             }
 
             return true;
         }
 
-        public void RunCmd(string filename)
+        private void SetCreatedTimeOnInitialization(OnboardingRequest onboardingRequest)
         {
-            ProcessStartInfo startInfo = new ProcessStartInfo(CmdPath);
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            startInfo.Arguments = CmdConfigArgs
-                + SystemHelpers.CmdCopyArgs(SourcePath, DestPath, filename)
-                + SystemHelpers.CmdAddToDepotArgs(DestPath, filename);
-            Process process = Process.Start(startInfo);
+            onboardingRequest.CreatedDate = DateTime.UtcNow;
+            onboardingRequest.DisplayCreatedDate = onboardingRequest.CreatedDate.ToLocalTime().ToString("M/d/yyyy h:mm tt",
+                  CultureInfo.CreateSpecificCulture("en-US"));
+            onboardingRequest.ModifiedDate = onboardingRequest.CreatedDate;
+            onboardingRequest.DisplayModifiedDate = onboardingRequest.DisplayCreatedDate;
+        }
+
+        private string SetSptFilename(OnboardingRequest onboardingRequest)
+        {
+            return onboardingRequest.DisplayName + "_" + onboardingRequest.CreatedBy + ".xml";
+        }
+
+        private void SubmitCodeReviewFromRequest(OnboardingRequest onboardingRequest, string filename)
+        {
+            // Step 1 - Create a code review
+            // Assign ReviewId to the corresponding field in OnboardingRequest
+            onboardingRequest.CodeFlowId = CodeFlowHelpers.CreateReview(rClient, onboardingRequest.CreatedBy, "Chengkan Huang",
+                onboardingRequest.CreatedBy + "@microsoft.com", "Testing integration", "MSODS");
+
+            // Step 2 - Create a code package and add it to the review
+            CodeFlowHelpers.AddCodePackage(rClient, onboardingRequest.CodeFlowId, CodeFlowHelpers.CreateCodePackage("testing pack", onboardingRequest.CreatedBy, onboardingRequest.CreatedBy, CodePackageFormat.SourceDepotPack, new Uri(SystemHelpers.DestPath + filename)));
+            //CodeFlowHelpers.AddCodePackage(rClient, onboardingRequest.CodeFlowId, CodeFlowHelpers.CreateCodePackage("testing pack", onboardingRequest.CreatedBy, onboardingRequest.CreatedBy, new Uri(SystemHelpers.DestPath + filename)));
+
+
+            // Step 3 - Add reviewers to the review
+            CodeFlowHelpers.AddReviewers(rClient, onboardingRequest.CodeFlowId, new ReviewService.Reviewer[]
+                    {
+                        CodeFlowHelpers.CreateReviewer(onboardingRequest.CreatedBy, "Chengkan Huang", onboardingRequest.CreatedBy + "@microsoft.com", true)
+                    });
+
+            // Step 4 - Publish the review
+            CodeFlowHelpers.PublishReview(rClient, onboardingRequest.CodeFlowId, "meesage from author");
         }
     }
 }
--- Onboarding/Utils/SystemHelpers.cs
+++ Onboarding/Utils/SystemHelpers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Web;
@@ -9,6 +10,12 @@ namespace Onboarding.Utils
 {
     public static class SystemHelpers
     {
+        private const string CmdPath = @"C:\WINDOWS\system32\cmd.exe";
+        private const string CmdConfigArgs = @"/k set inetroot=e:\cumulus_main&set corextbranch=main&e:\cumulus_main\tools\path1st\myenv.cmd";
+        private const string SourcePath = @"C:\Users\t-chehu\Source\Repos\Onboarding\Onboarding\App_Data\";
+        public const string DestPath = @"E:\CUMULUS_MAIN\sources\dev\RestServices\GraphService\Tools\";
+        private const string SavingPathXml = @"../../App_Data/";
+
         /// <summary>
         /// Write string into an xml file and then optionally save it.
         /// </summary>
@@ -16,20 +23,30 @@ namespace Onboarding.Utils
         /// <param name="path">The saving path.</param>
         /// <param name="filename">Name of file</param>
         /// <returns>The xml in type of byte[]</returns>
-        public static byte[] SavesStringToXml(string xmlString, string path, string filename)
+        public static byte[] SavesStringToXml(string xmlString, string filename)
         {
             XmlDocument xdoc = new XmlDocument();
             xdoc.LoadXml(xmlString);
-            xdoc.Save(HttpContext.Current.Server.MapPath(path + filename));
+            xdoc.Save(HttpContext.Current.Server.MapPath(SavingPathXml + filename));
             return Encoding.Default.GetBytes(xdoc.OuterXml);
         }
 
-        public static string CmdCopyArgs(string source, string dest, string filename)
+        public static void RunCmd(string filename)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(CmdPath);
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = CmdConfigArgs
+                + CmdCopyArgs(SourcePath, DestPath, filename)
+                + CmdAddToDepotArgs(DestPath, filename);
+            Process process = Process.Start(startInfo);
+        }
+
+        private static string CmdCopyArgs(string source, string dest, string filename)
         {
             return " && copy " + source + filename + " " + dest;
         }
 
-        public static string CmdAddToDepotArgs(string depotPath, string filename)
+        private static string CmdAddToDepotArgs(string depotPath, string filename)
         {
             return " && cd " + depotPath + " && sd add " + filename;
         }
