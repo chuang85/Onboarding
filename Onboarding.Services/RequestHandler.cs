46b6f4f239f9c326f02e80a63af5e99b4e3646b5
Chengkan Huang <t-chehu@microsoft.com>
2014-07-10 20:15:57 -0700

    add back service(test version)

---
+++ Onboarding.Services/RequestHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Onboarding.Utils;
+using Onboarding.Utils.ReviewService;
+
+namespace Onboarding.Services
+{
+    public partial class RequestHandler : ServiceBase
+    {
+        public static ReviewServiceClient rClient = new ReviewServiceClient();
+        public RequestHandler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Step 1 - Create a code review
+            string reviewId = CodeFlowHelpers.CreateReview(rClient, "t-chehu", "Chengkan Huang", "t-chehu@microsoft.com", "Testing service", "MSODS");
+
+
+            // Step 2 - Create a code package and add it to the review
+            CodeFlowHelpers.AddCodePackage(rClient, reviewId, CodeFlowHelpers.CreateCodePackage("testing pack", "t-chehu", "t-chehu",
+                CodePackageFormat.SourceDepotPack, new Uri(@"E:\CUMULUS_MAIN\sources\dev\RestServices\GraphService\Tools\qwefd_t-chehu.xml.dpk")));
+
+
+            // Step 3 - Add reviewers to the review
+            CodeFlowHelpers.AddReviewers(rClient, reviewId, new Reviewer[]
+            {
+                CodeFlowHelpers.CreateReviewer("t-chehu", "Chengkan Huang", "t-chehu@microsoft.com", true)
+            });
+
+
+            // Step 4 - Publish the review
+            CodeFlowHelpers.PublishReview(rClient, reviewId, "meesage from author");
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
