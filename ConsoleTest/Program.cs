00df58bc636cc29afb2101de4d4f97620be147e5
Chengkan Huang <t-chehu@microsoft.com>
2014-07-13 17:22:22 -0700

    trying to connect db from serivce

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -13,57 +13,66 @@ using Reviewer = Onboarding.Utils.ReviewService.Reviewer;
 
 namespace ConsoleTest
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            var rClient = new ReviewServiceClient();
-            var qClient = new ReviewDashboardServiceClient();
             using (var db = new OnboardingDbContext())
             {
-                try
+                foreach (var r in (from d in db.OnboardingRequests select d))
                 {
-                    // Handling request in state "Created"
-                    foreach (var request in DbHelpers.RequestsCreated(db))
-                    {
-                        // Create a code review.
-                        var codeFlowId = CodeFlowHelpers.CreateReview(rClient, request.CreatedBy, "Chengkan Huang",
-                            CodeFlowHelpers.GenerateEmailAddress(request), CodeFlowHelpers.GenerateReivewName(request), CodeFlowHelpers.ProjectShortName);
-                        // Assign ReviewId to the corresponding field in OnboardingRequest
-                        request.CodeFlowId = codeFlowId;
-                        // Create a code package and add it to the review
-                        CodeFlowHelpers.AddCodePackage(rClient, request.CodeFlowId, CodeFlowHelpers.CreateCodePackage("testing pack", request.CreatedBy, request.CreatedBy,
-                            CodePackageFormat.SourceDepotPack, new Uri(SystemHelpers.DepotPath + CodeFlowHelpers.GenerateFilename(request) + ".dpk")));
-                        // Add reviewers to the review
-                        CodeFlowHelpers.AddReviewers(rClient, request.CodeFlowId, new Reviewer[]
-                        {
-                            CodeFlowHelpers.CreateReviewer(request.CreatedBy, "Chengkan Huang", CodeFlowHelpers.GenerateEmailAddress(request), true)
-                        });
-                        // Publish the review
-                        CodeFlowHelpers.PublishReview(rClient, request.CodeFlowId, "meesage from author");
-                        // Change State from "Created" to "PendingReview"
-                        request.State = "PendingReview";
-                    }
-                    // Handling request in state "PendingReview"
-                    foreach (var request in DbHelpers.RequestsPendingReview(db))
-                    {
-                        if (CodeFlowHelpers.ReviewCompleted(qClient, request.CodeFlowId, request.CreatedBy))
-                        {
-                            request.State = "ReviewApproved";
-                        }
-                    }
-                    db.SaveChanges();
+                    r.DisplayName = "shitty";
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("An error occurred: " + ex.Message);
-                }
+                db.SaveChanges();
             }
-            Console.WriteLine("done");
-            rClient.Close();
+            //var rClient = new ReviewServiceClient();
+            //var qClient = new ReviewDashboardServiceClient();
+            //using (var db = new OnboardingDbContext())
+            //{
+            //    try
+            //    {
+            //        // Handling request in state "Created"
+            //        foreach (var request in DbHelpers.RequestsCreated(db))
+            //        {
+            //            // Create a code review.
+            //            var codeFlowId = CodeFlowHelpers.CreateReview(rClient, request.CreatedBy, "Chengkan Huang",
+            //                CodeFlowHelpers.GenerateEmailAddress(request), CodeFlowHelpers.GenerateReivewName(request), CodeFlowHelpers.ProjectShortName);
+            //            // Assign ReviewId to the corresponding field in OnboardingRequest
+            //            request.CodeFlowId = codeFlowId;
+            //            // Create a code package and add it to the review
+            //            CodeFlowHelpers.AddCodePackage(rClient, request.CodeFlowId, CodeFlowHelpers.CreateCodePackage("testing pack", request.CreatedBy, request.CreatedBy,
+            //                CodePackageFormat.SourceDepotPack, new Uri(SystemHelpers.DepotPath + CodeFlowHelpers.GenerateFilename(request) + ".dpk")));
+            //            // Add reviewers to the review
+            //            CodeFlowHelpers.AddReviewers(rClient, request.CodeFlowId, new Reviewer[]
+            //            {
+            //                CodeFlowHelpers.CreateReviewer(request.CreatedBy, "Chengkan Huang", CodeFlowHelpers.GenerateEmailAddress(request), true)
+            //            });
+            //            // Publish the review
+            //            CodeFlowHelpers.PublishReview(rClient, request.CodeFlowId, "meesage from author");
+            //            // Change State from "Created" to "PendingReview"
+            //            request.State = "PendingReview";
+            //        }
+            //        // Handling request in state "PendingReview"
+            //        foreach (var request in DbHelpers.RequestsPendingReview(db))
+            //        {
+            //            if (CodeFlowHelpers.ReviewCompleted(qClient, request.CodeFlowId, request.CreatedBy))
+            //            {
+            //                request.State = "ReviewApproved";
+            //            }
+            //        }
+            //        db.SaveChanges();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine("An error occurred: " + ex.Message);
+            //    }
+            //}
+            //rClient.Close();
+            //qClient.Close();
+            //Console.WriteLine("done");
 
-            Console.WriteLine("Hit enter...");
-            Console.Read();
+            //Console.WriteLine("Hit enter...");
+            //Console.Read();
         }
     }
 }
--- Onboarding.Services/RequestHandler.cs
+++ Onboarding.Services/RequestHandler.cs
@@ -4,47 +4,136 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.AccessControl;
 using System.ServiceProcess;
 using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 using Onboarding.Models;
 using Onboarding.Utils;
 using Onboarding.Utils.ReviewService;
+using Onboarding.Utils.DashboardService;
+using Reviewer = Onboarding.Utils.ReviewService.Reviewer;
 
 namespace Onboarding.Services
 {
     public partial class RequestHandler : ServiceBase
     {
-        private static ReviewServiceClient _rClient;
+        private ReviewServiceClient _rClient;
+        private ReviewDashboardServiceClient _qClient;
+        private Timer timer;
         public RequestHandler()
         {
             InitializeComponent();
-            _rClient = new ReviewServiceClient();
         }
 
         protected override void OnStart(string[] args)
         {
-            using (var db = new OnboardingDbContext())
-            {
-                try
-                {
-                    foreach (var request in DbHelpers.RequestsCreated(db))
-                    {
-                        //CodeFlowHelpers.SubmitCodeReviewFromRequest(db, _rClient, request);
+            _rClient = new ReviewServiceClient();
+            _qClient = new ReviewDashboardServiceClient();
+            timer = new Timer(5000D);
+            timer.AutoReset = true;
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            timer.Start();
 
-                    }
-                    db.SaveChanges();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("An error occurred: " + ex.Message);
-                }
-            }
+            //using (var db = new OnboardingDbContext())
+            //{
+            //    try
+            //    {
+            //        // Handling request in state "Created"
+            //        foreach (var request in DbHelpers.RequestsCreated(db))
+            //        {
+            //            // Create a code review.
+            //            var codeFlowId = CodeFlowHelpers.CreateReview(_rClient, request.CreatedBy, "Chengkan Huang",
+            //                CodeFlowHelpers.GenerateEmailAddress(request), CodeFlowHelpers.GenerateReivewName(request),
+            //                CodeFlowHelpers.ProjectShortName);
+            //            // Assign ReviewId to the corresponding field in OnboardingRequest
+            //            request.CodeFlowId = codeFlowId;
+            //            // Create a code package and add it to the review
+            //            CodeFlowHelpers.AddCodePackage(_rClient, request.CodeFlowId,
+            //                CodeFlowHelpers.CreateCodePackage("testing pack", request.CreatedBy, request.CreatedBy,
+            //                    CodePackageFormat.SourceDepotPack,
+            //                    new Uri(SystemHelpers.DepotPath + CodeFlowHelpers.GenerateFilename(request) + ".dpk")));
+            //            // Add reviewers to the review
+            //            CodeFlowHelpers.AddReviewers(_rClient, request.CodeFlowId, new Reviewer[]
+            //            {
+            //                CodeFlowHelpers.CreateReviewer(request.CreatedBy, "Chengkan Huang",
+            //                    CodeFlowHelpers.GenerateEmailAddress(request), true)
+            //            });
+            //            // Publish the review
+            //            CodeFlowHelpers.PublishReview(_rClient, request.CodeFlowId, "meesage from author");
+            //            // Change State from "Created" to "PendingReview"
+            //            request.State = "PendingReview";
+            //        }
+            //        // Handling request in state "PendingReview"
+            //        foreach (var request in DbHelpers.RequestsPendingReview(db))
+            //        {
+            //            if (CodeFlowHelpers.ReviewCompleted(_qClient, request.CodeFlowId, request.CreatedBy))
+            //            {
+            //                request.State = "ReviewApproved";
+            //            }
+            //        }
+            //        //DoWork(db);
+            //        db.SaveChanges();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine("An error occurred: " + ex.Message);
+            //    }
+            //}
         }
 
         protected override void OnStop()
         {
             _rClient.Close();
+            _qClient.Close();
+            timer.Stop();
+            timer = null;
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            ConsoleTest.Program.Main();
+        }
+
+        private void DoWork(OnboardingDbContext db)
+        {
+            HandleCreated(db);
+            HandlePendingReview(db);
+        }
+
+        private void HandleCreated(OnboardingDbContext db)
+        {
+            foreach (var request in DbHelpers.RequestsCreated(db))
+            {
+                // Create a code review.
+                var codeFlowId = CodeFlowHelpers.CreateReview(_rClient, request.CreatedBy, "Chengkan Huang",
+                    CodeFlowHelpers.GenerateEmailAddress(request), CodeFlowHelpers.GenerateReivewName(request), CodeFlowHelpers.ProjectShortName);
+                // Assign ReviewId to the corresponding field in OnboardingRequest
+                request.CodeFlowId = codeFlowId;
+                // Create a code package and add it to the review
+                CodeFlowHelpers.AddCodePackage(_rClient, request.CodeFlowId, CodeFlowHelpers.CreateCodePackage("testing pack", request.CreatedBy, request.CreatedBy,
+                    CodePackageFormat.SourceDepotPack, new Uri(SystemHelpers.DepotPath + CodeFlowHelpers.GenerateFilename(request) + ".dpk")));
+                // Add reviewers to the review
+                CodeFlowHelpers.AddReviewers(_rClient, request.CodeFlowId, new Reviewer[]
+                        {
+                            CodeFlowHelpers.CreateReviewer(request.CreatedBy, "Chengkan Huang", CodeFlowHelpers.GenerateEmailAddress(request), true)
+                        });
+                // Publish the review
+                CodeFlowHelpers.PublishReview(_rClient, request.CodeFlowId, "meesage from author");
+                // Change State from "Created" to "PendingReview"
+                request.State = "PendingReview";
+            }
+        }
+
+        private void HandlePendingReview(OnboardingDbContext db)
+        {
+            foreach (var request in DbHelpers.RequestsPendingReview(db))
+            {
+                if (CodeFlowHelpers.ReviewCompleted(_qClient, request.CodeFlowId, request.CreatedBy))
+                {
+                    request.State = "ReviewApproved";
+                }
+            }
         }
     }
 }
