edc5207b057267576bc099dc2f5c24460aee767a
Chengkan Huang <t-chehu@microsoft.com>
2014-07-21 15:21:07 -0700

    delete a js lib, add js helper

---
+++ Onboarding/App/services/savehelper.js
@@ -0,0 +1,16 @@
+﻿define(function() {
+    var vm = {
+        removeDomain: removeDomain
+    };
+
+    function removeDomain(raw) {
+        if (raw.indexOf("\\") > -1) {
+            var res = raw.split("\\");
+            return res[res.length - 1];
+        } else {
+            return raw;
+        }
+    }
+
+    return vm;
+});
